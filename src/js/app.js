a78ebc1713b87d9f81d6f2e36c78474b683a9988
Paul Hebert <paul@cloudfour.com>
2021-11-07 09:47:41 -0800

    Allow multiple word selection

--- src/js/app.js
+++ src/js/app.js
@@ -2,30 +2,43 @@ import { randomItemFromArray, shuffle } from "./utils.js";
 import { anagrams } from "./generated/anagrams.js";
 const anagramKeys = Object.keys(anagrams);
 
-const container = document.querySelector(".anagram-container");
+const containerEl = document.querySelector(".anagram-container");
+const nextLevelButtonEl = document.querySelector(".js-next-level");
 const currentLevelEl = document.querySelector(".js-current-level");
 const currentMovesEl = document.querySelector(".js-current-moves");
 const currentPointsEl = document.querySelector(".js-current-points");
+const wordsFoundEl = document.querySelector(".js-found-words");
+const wordsFoundCountEl = document.querySelector(".js-found-words-count");
 const possibleWordsEl = document.querySelector(".js-possible-words");
 
-const drake = dragula([container]);
+const drake = dragula([containerEl]);
 
 const state = {
-  level: 1,
+  level: 0,
   points: 0,
 };
 
 loadLevel();
 
-function loadLevel() {
-  state.movesLeft = 10;
-  currentMovesEl.textContent = state.movesLeft;
+nextLevelButtonEl.addEventListener("click", loadLevel);
 
+function loadLevel() {
+  state.level++;
+  state.movesLeft = 10;
+  state.wordsFound = [];
   currentLevelEl.textContent = state.level;
+  currentMovesEl.textContent = state.movesLeft;
+  wordsFoundEl.textContent = "";
+  wordsFoundCountEl.textContent = state.wordsFound.length;
+  currentPointsEl.textContent = state.points;
+
+  nextLevelButtonEl.setAttribute("hidden", "");
 
   state.jumble = randomItemFromArray(anagramKeys);
   state.matches = anagrams[state.jumble];
-  possibleWordsEl.textContent = state.matches.length;
+  state.possibleWords = state.matches.length;
+  possibleWordsEl.textContent = state.possibleWords;
+
   console.log(state.jumble, state.matches);
 
   const jumbleArray = state.jumble.split("");
@@ -36,7 +49,7 @@ function loadLevel() {
     shuffledLetters = shuffle(jumbleArray).join("");
   }
 
-  container.innerHTML = shuffledLetters
+  containerEl.innerHTML = shuffledLetters
     .split("")
     .map(
       (letter, i) => /* html */ `
@@ -52,26 +65,36 @@ drake.on("dragend", () => {
   state.movesLeft--;
   currentMovesEl.textContent = state.movesLeft;
 
-  if (state.matches.includes(container.textContent.replace(/\s/g, ""))) {
-    container.querySelectorAll(".letter").forEach((letter, i) => {
+  const word = containerEl.textContent.replace(/\s/g, "");
+  const wordMatches = state.matches.includes(word);
+  const wordAlreadyUsed = state.wordsFound.includes(word);
+
+  if (wordMatches && !wordAlreadyUsed) {
+    containerEl.querySelectorAll(".letter").forEach((letter, i) => {
       letter.style.setProperty("--index", i);
     });
-    container.classList.add("is-success");
+    containerEl.classList.add("is-success");
 
     state.points++;
+    state.wordsFound.push(word);
     currentPointsEl.textContent = state.points;
+    wordsFoundEl.textContent = state.wordsFound.join(", ");
+    wordsFoundCountEl.textContent = state.wordsFound.length;
+
+    nextLevelButtonEl.removeAttribute("hidden");
 
     setTimeout(() => {
-      container.classList.remove("is-success");
+      containerEl.classList.remove("is-success");
       state.level++;
-      loadLevel();
+
+      if (state.wordsFound.length === state.possibleWords) {
+        loadLevel();
+      }
     }, 2000);
   } else if (state.movesLeft === 0) {
     alert("You lose!");
-    state = {
-      level: 1,
-      points: 0,
-    };
+    state.level = 0;
+    state.points = 0;
     loadLevel();
   }
 });
