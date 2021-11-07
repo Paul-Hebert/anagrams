dbef86125db0d66797e18446955f4b6f3a7bb180
Paul Hebert <paul@cloudfour.com>
2021-11-07 09:23:03 -0800

    Implement core game logic

--- src/js/app.js
+++ src/js/app.js
@@ -1,24 +1,38 @@
+import { randomItemFromArray, shuffle } from "./utils.js";
 import { anagrams } from "./generated/anagrams.js";
-
 const anagramKeys = Object.keys(anagrams);
 
 const container = document.querySelector(".anagram-container");
-const reloadButton = document.querySelector(".reload");
+const currentLevelEl = document.querySelector(".js-current-level");
+const currentMovesEl = document.querySelector(".js-current-moves");
+const currentPointsEl = document.querySelector(".js-current-points");
+const possibleWordsEl = document.querySelector(".js-possible-words");
 
 const drake = dragula([container]);
 
-load();
+const state = {
+  level: 1,
+  points: 0,
+};
 
-function load() {
-  const jumble = randomItemFromArray(anagramKeys);
-  const matches = anagrams[jumble];
-  console.log(jumble, matches);
+loadLevel();
 
-  const jumbleArray = jumble.split("");
+function loadLevel() {
+  state.movesLeft = 10;
+  currentMovesEl.textContent = state.movesLeft;
+
+  currentLevelEl.textContent = state.level;
+
+  state.jumble = randomItemFromArray(anagramKeys);
+  state.matches = anagrams[state.jumble];
+  possibleWordsEl.textContent = state.matches.length;
+  console.log(state.jumble, state.matches);
+
+  const jumbleArray = state.jumble.split("");
 
   let shuffledLetters = shuffle(jumbleArray).join("");
 
-  while (matches.includes(shuffledLetters)) {
+  while (state.matches.includes(shuffledLetters)) {
     shuffledLetters = shuffle(jumbleArray).join("");
   }
 
@@ -32,42 +46,32 @@ function load() {
       `
     )
     .join("");
-
-  drake.on("dragend", () => {
-    if (matches.includes(container.textContent.replace(/\s/g, ""))) {
-      container.querySelectorAll(".letter").forEach((letter, i) => {
-        letter.style.setProperty("--index", i);
-      });
-      container.classList.add("is-success");
-
-      setTimeout(() => {
-        container.classList.remove("is-success");
-        load();
-      }, 2000);
-    }
-  });
 }
 
-function randomItemFromArray(array) {
-  return array[Math.floor(Math.random() * array.length)];
-}
+drake.on("dragend", () => {
+  state.movesLeft--;
+  currentMovesEl.textContent = state.movesLeft;
 
-function shuffle(array) {
-  let currentIndex = array.length,
-    randomIndex;
+  if (state.matches.includes(container.textContent.replace(/\s/g, ""))) {
+    container.querySelectorAll(".letter").forEach((letter, i) => {
+      letter.style.setProperty("--index", i);
+    });
+    container.classList.add("is-success");
 
-  // While there remain elements to shuffle...
-  while (currentIndex != 0) {
-    // Pick a remaining element...
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex--;
+    state.points++;
+    currentPointsEl.textContent = state.points;
 
-    // And swap it with the current element.
-    [array[currentIndex], array[randomIndex]] = [
-      array[randomIndex],
-      array[currentIndex],
-    ];
+    setTimeout(() => {
+      container.classList.remove("is-success");
+      state.level++;
+      loadLevel();
+    }, 2000);
+  } else if (state.movesLeft === 0) {
+    alert("You lose!");
+    state = {
+      level: 1,
+      points: 0,
+    };
+    loadLevel();
   }
-
-  return array;
-}
+});
