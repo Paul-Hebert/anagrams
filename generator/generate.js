0cb8796b7f48654b76a587b30ccf6e6c44c344fa
Paul Hebert <paul@cloudfour.com>
2021-11-13 10:41:27 -0800

    Track generator output to simplify Netlify integration

--- generator/generate.js
+++ generator/generate.js
@@ -2,8 +2,6 @@ const fs = require("fs");
 const mergeLetters = require("./utils/merge-letters.js");
 const findAnagrams = require("./utils/find-anagrams.js");
 
-console.log(process.cwd());
-
 /**
  * Settings
  */
